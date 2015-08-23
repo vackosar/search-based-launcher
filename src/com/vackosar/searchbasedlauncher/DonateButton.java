a1671cacb8ec5bbfd031b94078619a33e254e88c
vakosar <vackosar>
2015-08-23 12:48:23 +0200

    Remove explicit link to Paypal.

--- src/com/vackosar/searchbasedlauncher/DonateButton.java
+++ src/com/vackosar/searchbasedlauncher/DonateButton.java
@@ -22,7 +22,7 @@ public class DonateButton implements Colorful, View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=vackosar%40gmail%2ecom&lc=US&item_name=ideasfrombrain&item_number=Search%20based%20launcher&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest"));
+        Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/vackosar/search-based-launcher/wiki/Donate"));
         marketIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         mainActivity.startActivity(marketIntent);
     }
