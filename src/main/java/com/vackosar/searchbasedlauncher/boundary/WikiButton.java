9ed051b527c0aad609d4cd1e67893cf8033a197e
vakosar <vackosar>
2015-10-12 22:21:18 +0200

    #18 Move Wiki button to Menu.

--- src/main/java/com/vackosar/searchbasedlauncher/boundary/WikiButton.java
+++ src/main/java/com/vackosar/searchbasedlauncher/boundary/WikiButton.java
@@ -4,30 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.google.inject.Inject;
 import com.vackosar.searchbasedlauncher.R;
-import com.vackosar.searchbasedlauncher.control.Colorful;
 
 import roboguice.context.event.OnCreateEvent;
 import roboguice.event.Observes;
 import roboguice.inject.InjectView;
 
-public class WikiButton extends Colorful implements View.OnClickListener{
+public class WikiButton implements View.OnClickListener{
 
-    @InjectView(R.id.wikiButton) private TextView textView;
+    @InjectView(R.id.wikiButton) private Button button;
     @Inject private Activity activity;
 
     public static final String WIKI_URI = "https://github.com/vackosar/search-based-launcher/wiki";
 
     public void onCreateEvent(@Observes OnCreateEvent onCreateEvent) {
-        textView.setOnClickListener(this);
-    }
-
-    @Override
-    public TextView getView() {
-        return null;
+        button.setOnClickListener(this);
     }
 
     @Override
